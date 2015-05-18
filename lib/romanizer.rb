8de25f72898f33ddd7202fb04a7636c12458ec2d
jarkyn <jarkyn@ymail.com>
2015-05-18 11:43:46 +0100

    removes duplication

--- lib/romanizer.rb
+++ lib/romanizer.rb
@@ -1,7 +1,5 @@
 class Romanizer
   def self.convert(arabic)
-    return 'I' if arabic == 1
-    return 'I' + 'I' if arabic == 1 + 1
-    return 'I' + 'I' + 'I' if arabic == 1 + 1 + 1
+    return 'I' * arabic
   end
 end
