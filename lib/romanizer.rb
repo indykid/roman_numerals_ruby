21b0f5fc2f0a8e33d64c97bd9a050d10f75bc15a
jarkyn <jarkyn@ymail.com>
2015-05-18 15:27:33 +0100

    removes redundant code

--- lib/romanizer.rb
+++ lib/romanizer.rb
@@ -9,9 +9,9 @@ class Romanizer
   def self.convert(arabic)
     if CONVERSIONS.keys.include?(arabic)
       return CONVERSIONS[arabic]
+    else
+
     end
-    return 'X' if arabic == 10
-    return 'V' if arabic == 5
 
     return 'V' + 'I' if arabic == 5 + 1
     return 'V' + 'I' + 'I' if arabic == 5 + 1 + 1
