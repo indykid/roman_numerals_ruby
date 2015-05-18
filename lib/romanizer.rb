6d1ec5beadabcb4ba1a6922d9c0664870ebdc70c
jarkyn <jarkyn@ymail.com>
2015-05-18 15:20:45 +0100

    attempts to group code for numbers between 5 and 10

--- lib/romanizer.rb
+++ lib/romanizer.rb
@@ -1,12 +1,15 @@
 class Romanizer
-  def self.convert(arabic)
-      return 'X' if arabic == 10
-      return 'V' if arabic == 5
 
+  def self.convert(arabic)
+    return 'X' if arabic == 10
+    return 'V' if arabic == 5
+
+    if (5...10).include?(arabic)
       return 'V' + 'I' if arabic == 5 + 1
       return 'V' + 'I' + 'I' if arabic == 5 + 1 + 1
       return 'V' + 'I' + 'I' + 'I' if arabic == 5 + 1 + 1 + 1
 
-      return 'I' * arabic
+    end
+    return 'I' * arabic
   end
 end
