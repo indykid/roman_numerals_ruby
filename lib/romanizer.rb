9b1c3c63ecb5bb64f7d634ff468a374157390605
jarkyn <jarkyn@ymail.com>
2015-05-18 15:08:58 +0100

    attempts to group some code

--- lib/romanizer.rb
+++ lib/romanizer.rb
@@ -4,13 +4,16 @@ class Romanizer
     return 'D' if arabic == 500
     return 'C' if arabic == 100
     return 'L' if arabic == 50
-    return 'X' if arabic == 10
-    return 'V' if arabic == 5
-    
-    return 'V' + 'I' if arabic == 5 + 1
-    return 'V' + 'I' + 'I' if arabic == 5 + 1 + 1
-    return 'V' + 'I' + 'I' + 'I' if arabic == 5 + 1 + 1 + 1
 
-    return 'I' * arabic
+    if arabic <= 10
+    return 'X' if arabic == 10
+      return 'V' if arabic == 5
+
+      return 'V' + 'I' if arabic == 5 + 1
+      return 'V' + 'I' + 'I' if arabic == 5 + 1 + 1
+      return 'V' + 'I' + 'I' + 'I' if arabic == 5 + 1 + 1 + 1
+
+      return 'I' * arabic
+    end
   end
 end
