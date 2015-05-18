2456a55dd46508d2ccb5ff0860cbb910c2e97b57
jarkyn <jarkyn@ymail.com>
2015-05-18 15:31:06 +0100

    introduces local variable result

--- lib/romanizer.rb
+++ lib/romanizer.rb
@@ -7,16 +7,18 @@ class Romanizer
   }
 
   def self.convert(arabic)
+    result = ''
+    
     if CONVERSIONS.keys.include?(arabic)
-      return CONVERSIONS[arabic]
+      result << CONVERSIONS[arabic]
     else
+      return 'V' + 'I' if arabic == 5 + 1
+      return 'V' + 'I' + 'I' if arabic == 5 + 1 + 1
+      return 'V' + 'I' + 'I' + 'I' if arabic == 5 + 1 + 1 + 1
 
+      return 'I' * arabic
     end
 
-    return 'V' + 'I' if arabic == 5 + 1
-    return 'V' + 'I' + 'I' if arabic == 5 + 1 + 1
-    return 'V' + 'I' + 'I' + 'I' if arabic == 5 + 1 + 1 + 1
-
-    return 'I' * arabic
+    result
   end
 end
