566624e60595ed2d8eef39e2c9489783a6849101
jarkyn <jarkyn@ymail.com>
2015-05-18 15:23:07 +0100

    introduces data structure to hold known conversions

--- lib/romanizer.rb
+++ lib/romanizer.rb
@@ -1,15 +1,22 @@
 class Romanizer
 
+  CONVERSIONS = {
+    1  => 'I',
+    5  => 'V',
+    10 => 'X'
+  }
+
   def self.convert(arabic)
+    if CONVERSIONS.keys.include?(arabic)
+      return CONVERSIONS[arabic]
+    end
     return 'X' if arabic == 10
     return 'V' if arabic == 5
 
-    if (5...10).include?(arabic)
-      return 'V' + 'I' if arabic == 5 + 1
-      return 'V' + 'I' + 'I' if arabic == 5 + 1 + 1
-      return 'V' + 'I' + 'I' + 'I' if arabic == 5 + 1 + 1 + 1
+    return 'V' + 'I' if arabic == 5 + 1
+    return 'V' + 'I' + 'I' if arabic == 5 + 1 + 1
+    return 'V' + 'I' + 'I' + 'I' if arabic == 5 + 1 + 1 + 1
 
-    end
     return 'I' * arabic
   end
 end
