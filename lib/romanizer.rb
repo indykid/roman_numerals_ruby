5630a34de66f2e9b01e860bda2ead55f158d083f
jarkyn <jarkyn@ymail.com>
2015-05-18 11:27:02 +0100

    converts 2 into II

--- lib/romanizer.rb
+++ lib/romanizer.rb
@@ -1,5 +1,6 @@
 class Romanizer
   def self.convert(arabic)
+    return 'II' if arabic == 2
     'I'
   end
 end
--- spec/romanizer_spec.rb
+++ spec/romanizer_spec.rb
@@ -3,4 +3,8 @@ describe Romanizer do
   it 'converts 1 to I' do
     expect(Romanizer.convert(1)).to eq('I')
   end
+
+  it 'converts 2 to II' do
+    expect(Romanizer.convert(2)).to eq('II')
+  end
 end
