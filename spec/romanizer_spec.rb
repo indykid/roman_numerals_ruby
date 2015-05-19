c5f3b47d56e4a165fa16d5aa8595ef6e41cd7348
jarkyn <jarkyn@ymail.com>
2015-05-19 14:54:35 +0100

    deletes code

--- spec/romanizer_spec.rb
+++ spec/romanizer_spec.rb
@@ -1,43 +1,32 @@
 describe 'romanizer' do
-  it 'converts 1' do
+  xit 'converts 1' do
     expect(romanizer(1)).to eq('I')
   end
 
-  it 'converts 2' do
+  xit 'converts 2' do
     expect(romanizer(2)).to eq('II')
   end
 
-  it 'converts 3' do
+  xit 'converts 3' do
     expect(romanizer(3)).to eq('III')
   end
 
-  it 'converts 5' do
+  xit 'converts 5' do
     expect(romanizer(5)).to eq('V')
   end
 
-  it 'converts 6' do
+  xit 'converts 6' do
     expect(romanizer(6)).to eq('VI')
   end
 
-  it 'converts 7' do
+  xit 'converts 7' do
     expect(romanizer(7)).to eq('VII')
   end
 
-  it 'converts 8' do
+  xit 'converts 8' do
     expect(romanizer(8)).to eq('VIII')
   end
 end
 
 def romanizer(arabic)
-  result = ''
-  result << 'V' if arabic == 5
-  result << 'V' << 'I' if arabic == 5 + 1
-  result << 'V' << 'I' << 'I' if arabic == 5 + 1 + 1
-  result << 'V' << 'I' << 'I' << 'I' if arabic == 5 + 1 + 1 + 1
-  if arabic < 5
-    arabic.times do
-      result << 'I'
-    end
-  end
-  result
 end
